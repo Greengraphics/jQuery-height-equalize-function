9647add0c5a9985e86bac28811e9fe0f7a3bd638
Nathan Gearing <natgearing@gmail.com>
2016-08-23 16:40:04 +1000

    Awesome

--- equalize.js
+++ equalize.js
@@ -1,16 +1,19 @@
 (function($) {
+    'use strict';
 
-
-    function equalHeights(){
+    function equalHeights() {
+        var i;
         var groups = [];
         $('[data-equalize]').each(function() {
             var group = $(this).data('equalize');
 
             // Check if this group already exists
+            // If not add this group to the array
             if($.inArray(group,groups) == -1)
                 groups.push(group);
         });
 
+        // Loop through the groups and get max height
         for(i=0; groups.length>i; i++) {
             var elementGroup = $('[data-equalize='+groups[i]+']');
             var maxHeight = Math.max.apply(null, elementGroup.map(function () 
@@ -19,21 +22,19 @@
                 }
             ));
 
+            // Set height for the group elements
             elementGroup.css('min-height', maxHeight);
-
-            //console.log(groups[i]);
         }
-        //console.log(groups);
     }
     $(document).ready(equalHeights);
 
+    // Heights may change when text wraps another line.
     //$(window).resize(equalHeights);
     // Use throttling instead
     var doit;
     window.onresize = function(){
         clearTimeout(doit);
-        doit = setTimeout(reorderForMobile, 100);
-    }
-
+        doit = setTimeout(equalHeights, 100);
+    };
 
 })(jQuery);
